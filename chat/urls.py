55005456aa199176862143897f6f7ca1aa06a9f7
Keonhui Seo <lagonee23@gmail.com>
2023-12-20 22:43:53 +0900

    MTV 설계

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
 from . import views
 
+
+app_name = "chat"  # URL 해석시 사용할 namespace 이름
+
+# "/chat/"으로 시작하는 URL 패턴
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
