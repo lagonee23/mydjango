2f633fc154bd6997c75c6977ece932a2f734e19f
Keonhui Seo <lagonee23@gmail.com>
2023-12-25 13:38:11 +0900

    Add HTML file to template

--- board/urls.py
+++ board/urls.py
@@ -1,1 +1,10 @@
 from django.urls import path
+from . import views
+
+
+app_name = "board"  # URL 해석시 사용할 namespace 이름
+
+# "/board/"으로 시작하는 URL 패턴
+urlpatterns = [
+    path("", views.index, name='index')
+]
