b558f11ffdf9d59f1c15baa9ba56d59f981c4e1b
Keonhui Seo <lagonee23@gmail.com>
2024-03-22 22:01:10 +0900

    Add a Procfile for Elastic Beanstalk deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-cbnuyc24.ap-northeast-2.elasticbeanstalk.com']
 
 
 # Application definition
