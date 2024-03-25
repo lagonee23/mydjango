25efcc7f096ee673866e6eb96d225d00a2a9cae8
Keonhui Seo <lagonee23@gmail.com>
2024-03-25 23:00:28 +0900

    Add Readme.md file

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',    
     
-    'board.apps.BoardConfig',
     'minesweeper.apps.MinesweeperConfig',  # 앱 등록
 ]
 
