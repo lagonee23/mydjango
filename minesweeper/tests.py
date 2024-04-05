efdb610a14e020f3c91b734e85bc72b08ea35c09
Keonhui Seo <lagonee23@gmail.com>
2024-04-05 22:57:42 +0900

    test: 인덱스 뷰에 대한 테스트 코드 작성

--- minesweeper/tests.py
+++ minesweeper/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index_view_status_code(self):
+        response = self.client.get("/ko/")
+        self.assertEqual(response.status_code, 200)    
+
+    def test_polygon_points_format(self):
+        response = self.client.get(reverse('minesweeper:index'))
+        polygons = response.context['polygons']
+        print("-"*69)
+        print("Response :", response)
+        for polygon in polygons:
+            # points 형식 검증
+            points = polygon['points'].split(' ')
+            self.assertEqual(len(points), 6)  # 6개의 x, y 쌍이 있어야 함(육각형)
+            # ID 포맷 검증
+            polygon_id = polygon['polygon_id']
+            self.assertTrue('-' in polygon_id)  # ID에 '-'가 포함되어 있는지
--- minesweeper/urls.py
+++ minesweeper/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = "minesweeper"  # URL 해석시 사용할 namespace 이름
 
-# "/minesweeper/"으로 시작하는 URL 패턴
 urlpatterns = [
     path("", views.index, name="index"),
 ]
