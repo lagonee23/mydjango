8ca12bbcb5dbd7747d3737e1e222559e63cef6ca
Keonhui Seo <lagonee23@gmail.com>
2024-04-10 18:36:16 +0900

    feat: 네비게이션 바 완성

--- minesweeper/views.py
+++ minesweeper/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render
 
 # /minesweeper/
 def index(request):
-    x_len, y_len = 29, 15    # 가로,세로 길이(29, 15)
+    x_len, y_len = 33, 17    # 가로,세로 길이(29, 15)
     polygons = []
     for y in range(y_len):
         for x in range(x_len - (y % 2)):
             points = "{},{} {},{} {},{} {},{} {},{} {},{}".format(
-                15 + 7 * (y % 2) + 14 * x, 3 + 12 * y,
-                22 + 7 * (y % 2) + 14 * x, 7 + 12 * y,
-                22 + 7 * (y % 2) + 14 * x, 15 + 12 * y,
-                15 + 7 * (y % 2) + 14 * x, 19 + 12 * y,
-                8 + 7 * (y % 2) + 14 * x, 15 + 12 * y,
-                8 + 7 * (y % 2) + 14 * x, 7 + 12 * y
+                16 + 7 * (y % 2) + 14 * x, 1 + 12 * y,
+                23 + 7 * (y % 2) + 14 * x, 5 + 12 * y,
+                23 + 7 * (y % 2) + 14 * x, 13 + 12 * y,
+                16 + 7 * (y % 2) + 14 * x, 17 + 12 * y,
+                9 + 7 * (y % 2) + 14 * x, 13 + 12 * y,
+                9 + 7 * (y % 2) + 14 * x, 5 + 12 * y
             )
             polygon_id = f"{x}-{y}"
             polygons.append({"points":points, "polygon_id":polygon_id})
